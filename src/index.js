2487a999fe3bb669bc29ff2dd3450e3ec1446cc3
Irina Kramer <irakramer@gmail.com>
2020-04-19 00:14:28 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Game from './Game';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const StarMatch = () => {
+  const [gameId, setGameId] = useState(1);
+  // unmount the previous game, clear side effects and mount a new game
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<StarMatch />, document.getElementById('root'));
+
