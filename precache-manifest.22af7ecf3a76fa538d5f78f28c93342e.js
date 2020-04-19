4aad770848a046662ad3abe09d86f5f45ae13026
Irina Kramer <irakramer@gmail.com>
2020-04-19 00:13:15 -0700

    Updates

---
+++ precache-manifest.22af7ecf3a76fa538d5f78f28c93342e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00a6f516725d9316f59f08369105ccb",
+    "url": "/star-match-game/index.html"
+  },
+  {
+    "revision": "62f863aaab226e5abde4",
+    "url": "/star-match-game/static/css/main.36d36dea.chunk.css"
+  },
+  {
+    "revision": "05ffdaacbe8ca0bdea0e",
+    "url": "/star-match-game/static/js/2.9d0a61ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match-game/static/js/2.9d0a61ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f863aaab226e5abde4",
+    "url": "/star-match-game/static/js/main.f569f162.chunk.js"
+  },
+  {
+    "revision": "2a3f95b1f83a0f7f30d1",
+    "url": "/star-match-game/static/js/runtime-main.b001b03a.js"
+  }
+]);
